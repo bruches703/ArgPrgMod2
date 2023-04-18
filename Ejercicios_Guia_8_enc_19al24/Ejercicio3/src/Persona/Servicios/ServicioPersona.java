ef89ccf393a971b253b65f869a53482423170375
bruches703 <bruchesdavid@gmail.com>
2023-04-18 17:36:36 -0300

    Ejercicio 3 de guia 8 terminado

--- Ejercicios_Guia_8_enc_19al24/Ejercicio3/src/Persona/Servicios/ServicioPersona.java
+++ Ejercicios_Guia_8_enc_19al24/Ejercicio3/src/Persona/Servicios/ServicioPersona.java
@@ -13,7 +13,7 @@ import java.util.Scanner;
  * @author bruch
  */
 public class ServicioPersona {
-    Scanner leer=new Scanner(System.in);
+    Scanner leer=new Scanner(System.in).useDelimiter("\n");
     
     public boolean esMayorDeEdad(Persona persona){
         return persona.getEdad()>=18;
@@ -21,39 +21,43 @@ public class ServicioPersona {
     
     public Persona crearPersona(){
         String sexo;
+        
         System.out.println("Ingrese nombre: ");
-        String nombre=leer.nextLine();
+        String nombre=leer.next();
+        
         System.out.println("Ingrese edad: ");
         int edad=leer.nextInt();
+        
         System.out.println("Ingrese sexo H/M/O: ");       
-        sexo=leer.nextLine();
+        sexo=leer.next();
         while(!"h".equalsIgnoreCase(sexo) && !"m".equals(sexo) && !"o".equals(sexo)){
             System.out.println("Error, reingrese el sexo: ");
-            sexo=leer.nextLine();
+            sexo=leer.next();
         }
+        
         System.out.println("Ingrese peso:");
         double peso=leer.nextDouble();
+        
         System.out.println("Ingrese altura: ");
         double altura=leer.nextDouble();
         
+        System.out.println("\n\n");
         return new Persona(nombre, edad, sexo, peso, altura);
     }
     
     public int  calcularIMC(Persona persona){
         int opc;
-        int calculoPeso;
-        calculoPeso= (int) ((int)persona.getPeso()/(Math.pow(persona.getAltura(),2)));
+        double calculoPeso;
+        calculoPeso= (persona.getPeso()/(Math.pow(persona.getAltura(),2)));
         if(calculoPeso<20){
             opc=-1;
         }else if(calculoPeso>=20 || calculoPeso<=25){
             opc=0;
         }else{
-            opc=2;
+            opc=1;
         }
         return opc;
     }
     
-    public void mensajeDeMayoria(boolean esMayor){
-
-    }
+ 
 }
--- Ejercicios_Guia_8_enc_19al24/Ejercicio3/src/ejercicio3/Main.java
+++ Ejercicios_Guia_8_enc_19al24/Ejercicio3/src/ejercicio3/Main.java
@@ -20,12 +20,81 @@ public class Main {
     public static void main(String[] args) {
         // TODO code application logic here
         ServicioPersona nP = new ServicioPersona();
-        Persona persona = nP.crearPersona();
-        mensajeDeEdad(persona,nP);
-        calculoDePeso(persona, nP);
+        Persona[] persona = new Persona[4];        
+        boolean [] edad = new boolean[4];
+        int [] peso = new int[4];
         
+        //persona=cargarTodasLasPersonas(nP);
+        
+        persona[0]= new Persona("asd",21,"h",50,1.30);
+        persona[1]= new Persona("asd",10,"h",40,2.10);
+        persona[2]= new Persona("asd",11,"h",75,1.60);
+        persona[3]= new Persona("asd",25,"h",20,1.90);
+        
+        
+        edad=calcularMayoriaDeEdad(persona, nP);
+        peso=calcularIMCDePersonas(persona, nP);
+        
+        System.out.println("Porcentaje de mayoria de edad: "+porcentajeDeMayores(edad)+"%");
+        System.out.println("");
+        porcentajeDeIMCIdeal(peso);
+    }    
+    
+    
+    public static void porcentajeDeIMCIdeal(int[] peso){
+        int a=0,b=0,c=0;
+        for (int i = 0; i < peso.length; i++) {
+            switch (peso[i]) {
+                case -1:
+                    a++;
+                    break;
+                case 0:
+                    b++;
+                    break;
+                default:
+                    c++;
+                    break;
+            }
+        }
+        System.out.println("Porcentaje de personas baja de peso: "+a*100/peso.length);
+        System.out.println("Porcentaje de personas de peso correcto: "+b*100/peso.length);
+        System.out.println("Porcentaje de personas de sobre peso: "+c*100/peso.length);
     }
     
+    public static double porcentajeDeMayores(boolean[] edad){
+        int cont=0;
+        for (int i = 0; i < edad.length; i++) {
+            if(edad[i]){
+                cont++;
+            }
+        }
+        return (cont*100)/edad.length;
+    }
+    
+    public static int[] calcularIMCDePersonas(Persona[] persona, ServicioPersona np){
+        int vector[] = new int[4];
+        for (int i = 0; i < persona.length; i++) {
+            vector[i]=np.calcularIMC(persona[i]);
+        }
+        return vector;
+    }
+    
+    public static boolean[] calcularMayoriaDeEdad(Persona[] persona, ServicioPersona np){
+        boolean vector[] = new boolean[4];
+        for (int i = 0; i < 4; i++) {
+            vector[i]=np.esMayorDeEdad(persona[i]);
+        }
+        return vector;
+    }
+
+    public static Persona[] cargarTodasLasPersonas(ServicioPersona np){
+        Persona[] persona = new Persona[4];
+        for (int i = 0; i < persona.length; i++) {
+            persona[i]= np.crearPersona();
+        }
+        return persona;
+    }
+            
     public static void mensajeDeEdad(Persona persona, ServicioPersona nP){
         System.out.println("");
         if(nP.esMayorDeEdad(persona)){
